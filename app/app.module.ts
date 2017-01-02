0600a0d05f49491aff98d73b205c99fdecfd1ca0
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-02 06:49:22 -0800

    fix: i18n via ng2-translate

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,33 @@
-import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
 import {NativeScriptModule} from "nativescript-angular/platform";
+import {NativeScriptHttpModule} from "nativescript-angular/http";
 import {NativeScriptRouterModule} from "nativescript-angular/router";
+import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
+import {Http} from "@angular/http";
 import {AppRoutes, AppComponents} from "./app.routing";
 
 import {AppComponent} from "./app.component";
 
-import {TranslateModule} from 'ng2-translate';
+import {TranslateModule, TranslateLoader, TranslateStaticLoader} from 'ng2-translate';
+
+// for AoT compilation
+export function translateLoaderFactory(http: Http) {
+  return new TranslateStaticLoader(http, '/i18n', '.json');
+};
 
 @NgModule({
     declarations: [AppComponent, ...AppComponents],
     bootstrap: [AppComponent],
-    imports: [NativeScriptModule,
+    imports: [
+        NativeScriptModule,
+        NativeScriptHttpModule,
         NativeScriptRouterModule,
         NativeScriptRouterModule.forRoot(AppRoutes),
-        TranslateModule.forRoot()],
+        TranslateModule.forRoot([{
+            provide: TranslateLoader,
+            deps: [Http],
+            useFactory: (translateLoaderFactory)
+        }])
+    ],
     schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule {
